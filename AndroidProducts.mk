744cb3ea9b8a7b65e372c2b80196bc35dd2dbb9d
ArixElo <patroxgamer@gmail.com>
2021-10-06 17:21:02 +0200

    gauguin: ShapeShift-ify [1/2]
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_gauguin.mk
+    $(LOCAL_DIR)/ssos_gauguin.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_gauguin-user \
-    spark_gauguin-userdebug \
-    spark_gauguin-eng
+    ssos_gauguin-user \
+    ssos_gauguin-userdebug \
+    ssos_gauguin-eng
--- ssos_gauguin.mk
+++ ssos_gauguin.mk
@@ -7,17 +7,19 @@
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Spark stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common ShapeShiftOS stuff.
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_gauguin
+PRODUCT_NAME := ssos_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-SPARK_BUILD_TYPE := OFFICIAL
+# Inherit ShapeShiftOS common properties.
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.ssos.cpu=SD750G
