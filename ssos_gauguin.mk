45738484deb9fe9d26828da612f8346f340dc16f
ArixElo <patroxgamer@gmail.com>
2021-10-12 21:07:37 +0200

    gauguin: go official
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- ssos_gauguin.mk
+++ ssos_gauguin.mk
@@ -11,6 +11,7 @@ $(call inherit-product, device/xiaomi/gauguin/device.mk)
 $(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
+SSOS_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := ssos_gauguin
